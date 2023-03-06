1257ab768280605b2ab403a4566633533f7b7211
Margi <margipancholi7@gmail.com>
2023-03-06 10:09:31 -0500

    use of :' this'

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
-//classes
+//class basic 
+
+// class Car{
+//     drive(){
+
+//         console.log("Y r driving!");
+//     }
+//     break(){
+//         console.log("careful with break!");
+//     }
+// }
+// let car1 = new Car();
+// let car2 = new Car();
+// console.log(car1.drive());
+// console.log(car2.drive());
+//volume 
 class rectangularSolid{
     constructor(l,w,h){// l = Length w = Width h = Height
        this.l = l;
@@ -20,3 +35,32 @@ console.log("volume of rectangularSolid:" , total.volume);
 
 //create obj of class f
 //let object = new f();
+
+class Car{
+    constructor (model,year,color){
+        this.model =model;
+        this.year = year;
+        this.color =color;
+    }
+    whatisthis(){
+    return this;
+    }
+    drive(){
+        
+        console.log("Y r driving!");
+    }
+    break(){
+        console.log("careful with break!");
+    }
+}
+let car10 = new Car("AD200","2000","red" );
+let car20 = new Car("AD400","1998","black");
+console.log(car10.model);
+console.log(car10.color);
+console.log(car20.model);
+console.log(car20.color);
+console.log(car10.drive());
+console.log(car20.break());
+console.log(car20.whatisthis()); //this keyword reference to the current object that we are working with
+
+
